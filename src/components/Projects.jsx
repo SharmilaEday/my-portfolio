972674c3c0881af7f47b2a51fb0ab24c1df902af
SharmilaEday <sharmilaeday@gmail.com>
2026-03-19 12:19:59 -0500

    Change category format from string to array

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
         {
             id: 1,
             title: "Policy Intelligence Agent",
-            category: "GenAI","NLP",
+            category: ["GenAI","NLP"],
             desc: "Automated policy monitoring agent using n8n & Google Gemini, fetches real-time content, runs sentiment analysis and summarization, and delivers reports to Google Sheets and email with zero manual input.",
             tech: ["n8n", "API", "Agentic AI", "LLMs", "Guardrails"],
             icon: <Activity />,
@@ -20,7 +20,7 @@ const Projects = () => {
         {
             id: 2,
             title: "Automated IELTS Essay Scoring",
-            category: "GenAI", "NLP",
+            category: ["GenAI", "NLP"],
             desc: "Built an NLP pipeline for automated essay scoring with scalable inference over 1,000+ essays supporting low latency.",
             tech: ["Hugging Face", "Transformers", "ModernBERT", "Optuna"],
             icon: <MessageSquare />,
@@ -31,14 +31,14 @@ const Projects = () => {
         {
             id: 3,
             title: "Bird Species Classification",
-            category: "Computer Vision", "ML",
+            category: ["Computer Vision", "Machine Learning"],
             desc: "Classified endangered Hawaiian bird species from audio using EfficientNet on Mel-Spectrograms and XGBoost on geographic features and deployed via Dockerized Flask API on AWS EC2 with CI/CD.",
             tech: ["AWS", "Docker", "EfficientNet", "XGBoost"],
             icon: <Layout />,
             github: "https://github.com/SharmilaEday/BirdClef-Audio-Classification-Project",
             demo: "#",
             results: "65% Macro F1-Score. Sub-2s inference for 95% of requests. 30% faster deploys via automated pipeline."
-        }
+        },
         {
             id: 4,
             title: "Passenger Outcome Prediction",
@@ -49,40 +49,40 @@ const Projects = () => {
             github: "https://github.com/SharmilaEday/Passenger-Survival-Prediction-System",
             demo: "#",
             results: "16.5% accuracy boost via feature engineering. CatBoost reached 78.7% accuracy, 0.875 AUC-ROC."
-        }
+        },
         {
             id: 5,
             title: "Intelligent Van Routing Simulation",
-            category: "Machine Learning","Data Visualisation",
+            category: ["Machine Learning","Data Visualisation"],
             desc: "Multi-agent urban routing, dynamic fleet assignment under simulated high-demand conditions.",
             tech: ["Python"],
             icon: <Layout />,
-            github: "26% wait time reduction. 36% trip efficiency gain. Scaled to 8–10 requests/min.",
+            github: "https://github.com/SharmilaEday",
             demo: "#",
-            results: "39% validation loss reduction, 85% semantic accuracy & 40% faster training via CUDA"
-        }
+            results: "26% wait time reduction. 36% trip efficiency gain. Scaled to 8–10 requests/min."
+        },
         {
             id: 6,
             title: "Prompt Recovery using FLAN-T5",
-            category: "GenAI","NLP",
+            category: ["GenAI","NLP"],
             desc: "Built and deployed a Flask-based API with an HTML/CSS frontend delivering sub-2-second inference.",
             tech: ["FLAN-T5", "Hugging Face", "PyTorch", "CUDA"],
             icon: <Layout />,
             github: "https://github.com/SharmilaEday",
             demo: "#",
             results: "39% validation loss reduction, 85% semantic accuracy & 40% faster training via CUDA"
-        }
+        },
         {
             id: 7,
             title: "Value Iteration for Reinforcement Learning",
-            category: "Machine Learning", "Data Visualisation,
+            category: ["Machine Learning", "Data Visualisation"],
             desc: "Optimal policy learning in grid and graph-based MDPs, discounted reward analysis, obstacle handling, and custom Matplotlib policy visualizations.",
             tech: ["Python", "Matplotlib"],
             icon: <Layout />,
             github: "https://github.com/SharmilaEday",
             demo: "#",
             results: "12% policy value improvement. 40% better interpretability."
-        }
+        },
         {
             id: 8,
             title: "Network Flow Optimization",
